9767c4c77d2e02fb4bbc15e76e2e545692cd982d
Anefu <einsteinanefu@gmail.com>
2021-08-29 15:00:38 +0100

    edited main

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/prj19/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
+module "prj19" {
+  source  = "app.terraform.io/ATech/prj19/aws"
   version = "1.0.0"
+  name = var.name
+  prefix = var.prefix
+  region = var.region
 }
